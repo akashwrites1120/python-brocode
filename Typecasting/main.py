4f84ae9f334b6f3c417f102b4a2c2a83add1a110
Akash Maity <akashmaity4452@gmail.com>
2025-06-04 01:11:23 +0530

    typecasting

---
+++ Typecasting/main.py
@@ -0,0 +1,9 @@
+# Typecasting = the process of converting a variable from one data type to another 
+#               str(), int(), float(), bool()
+
+
+name = "Akash"
+age = 20
+gpa = 3.2
+is_student = True
+
