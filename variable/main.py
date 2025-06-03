458dd66f93d4713292f005d7922f8bc4b0607493
Akash Maity <akashmaity4452@gmail.com>
2025-06-04 01:05:52 +0530

    variable

---
+++ variable/main.py
@@ -0,0 +1,31 @@
+# variable  = A container for a value (string, integer, float, boolean)
+#             A variable behaves as if it was the value it containes
+
+# Strings
+first_name = "Akash"
+print(first_name)
+
+# Integers
+
+age = 25
+quantity = 3
+
+print(f"You are {age} years old and you are buying {quantity} items")
+
+
+# Float
+
+price = 10.99
+distance = 5.5
+
+print(f"You ran {distance} kilometers")
+
+# Boolean
+
+is_student = True
+
+if issubclass:
+    print("You are a student")
+else:
+    print("You are not a student")
+
