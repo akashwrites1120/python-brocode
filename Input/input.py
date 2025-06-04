464ecf3ef6144c1cd392b34563729ccbab42abf3
Akash Maity <akashmaity4452@gmail.com>
2025-06-05 00:04:00 +0530

    input exercise 1

---
+++ Input/input.py
@@ -0,0 +1,13 @@
+#input() = A function that prompts the user to enter data
+            # Returns the entered data as a string 
+
+name =  input("Enter your name: ")
+age = int(input("How old are you?: "))
+
+age = age+1
+
+print(f"Hello {name}!")
+print(f"You are {age} years old")
+
+
+
